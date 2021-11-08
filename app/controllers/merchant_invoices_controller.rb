9211d8d93eab3d4ea7b117cacce3f066cb9a0c9f
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-08 13:14:52 -0500

    Fix admin show created at test

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -10,4 +10,15 @@ class MerchantInvoicesController < ApplicationController
     @invoice_items = InvoiceItem.find_by(invoice_id: @invoice.id)
     @item = Item.find(@invoice_items.item_id)
   end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+    redirect_to "/merchants/#{merchant.id}/invoices/#{invoice.id}"
+  end
+
+  private
+  def invoice_params
+    params.require(:invoice).permit(:status)
+  end
 end
