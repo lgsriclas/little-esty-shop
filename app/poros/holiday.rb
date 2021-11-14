9a484bb4a749994313aaca2fcfa5fb96fd92d228
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-14 10:29:43 -0500

    Add holiday api to view

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -2,10 +2,10 @@ class Holiday
   attr_reader :holidays
 
   def initialize(data)
-    @holidays = data[:holidays]
+    @three = data[:holidays]
   end
 
   def upcoming_holidays(code)
-    @holidays.next_holidays(country_code[0..2])
+    @three.next_holidays(country_code[0..2])
   end
 end
