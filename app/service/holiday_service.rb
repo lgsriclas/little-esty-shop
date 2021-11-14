8e7ac6d5d872caf35a228fed56ea6cbcdc939279
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-14 12:03:32 -0500

    Update holiday service

--- app/service/holiday_service.rb
+++ app/service/holiday_service.rb
@@ -1,10 +1,6 @@
 class HolidayService
   def self.holiday
-    response = get_url('https://date.nager.at/').get('api/v2/NextPublicHolidays/us')
+    response = Faraday.get('https://date.nager.at/api/v2/NextPublicHolidays/us')
     JSON.parse(response.body, symbolize_names: true)[0..2]
-  end
-
-  def self.get_url(url)
-    Faraday.new(url)
   end
 end
